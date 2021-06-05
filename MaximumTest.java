6c289cba1c10e73a6d77e69d8020a9c4788d2d04
Ranjithgc <ranjith.amca.16@acharya.ac.in>
2021-06-05 13:21:39 +0530

    Test Cases for 3 Comparable Objects

--- MaximumTest.java
+++ MaximumTest.java
@@ -68,4 +68,39 @@ class MaximumTest {
         String maximumResult = maximum.checkMaximum("Ant", "Cat", "Dog");
         Assert.assertEquals("Dog", maximumResult);
     }
+
+ @Test
+    public void givenInteger_ShouldReturnMaximumInteger() {
+        Comparable maximumResult=maximum.checkMaximum(1,2,3);
+        Assert.assertEquals(3,maximumResult);
+    }
+    @Test
+    public void givenFloat_UsingGenericClass_ShouldReturnMaximumFloat() {
+        Comparable maximumResult=maximum.checkMaximum(1.1,2.2,3.3);
+        Assert.assertEquals(3.3,maximumResult);
+    }
+    @Test
+    public void givenString_UsingGenericClass_ShouldReturnMaximumString(){
+        Comparable maximumResult=maximum.checkMaximum("peach","orange","apple");
+        Assert.assertEquals("peach",maximumResult);
+    }
+    @Test
+    public void givenNIntegers_UsingMoreThanThreeParameters_ShouldReturnMaximumInteger() {
+        Integer maximumResult=maximum.checkMaximum_With_MoreThan_ThreeParameters(1,2,3,4,5,6);
+        Integer expectedResult=6;
+        Assert.assertEquals(expectedResult,maximumResult);
+    }
+    @Test
+    public void givenNFloats_UsingMoreThanThreeParameters_ShouldReturnMaximumFloat(){
+        Float maximumResult=maximum.checkMaximum_With_MoreThan_ThreeParameters(1.1f,2.2f,3.3f,4.4f,5.5f);
+        Float expectedResult=5.5f;
+        Assert.assertEquals(expectedResult,maximumResult);
+    }
+    @Test
+    public void givenNStrings_UsingMoreThanThreeParameters_ShouldReturnMaximumString(){
+        String maximumResult=maximum.checkMaximum_With_MoreThan_ThreeParameters("Dog","Cat","Ant","Tiger");
+        Assert.assertEquals("Tiger",maximumResult);
+    }
+
+}
 }
